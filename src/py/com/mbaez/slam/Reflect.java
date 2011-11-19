d38826cb8d46a4e3356a3491ad071b5fd5c51148
Maximiliano Báez González <mxbg.py@gmail.com>
2011-11-19 00:55:54 -0300

    se añadieron comentarios a las clases

--- src/py/com/mbaez/slam/Reflect.java
+++ src/py/com/mbaez/slam/Reflect.java
@@ -1,7 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
  */
+
 package py.com.mbaez.slam;
 
 import java.lang.reflect.Field;
@@ -9,27 +22,26 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- *
  * @author Maximiliano Báez <mxbg.py@gmail.com>
+ * @version 1.0 20/10/2011
  */
 public class Reflect {
-    
+
     /**
      *Constructor de la clase reflect
      */
-    private Refelct(){
+    public Reflect() {
     }
-
 
     /**
      *
      * Se obtien el nombre del atributo y se genera construye una cadena que
      * contiene el nombre del seter de dicho atributo. La cadena se construye de
      * la siguiente forma:
-     *
+     *<p>
      * seterName = la cadena "set" + el primer caracter de fieldName en
      * mayusculas + los caracteres restantates de la cadena fieldName.
-     *
+     *<p>
      * La construcción se realiza de esta respentando la nomenclatura camelCase.
      *
      * @param fieldName
@@ -42,16 +54,16 @@ public class Reflect {
                 + fieldName.substring(1);
         return seterName;
     }
-    
+
     /**
      *
      * Se obtien el nombre del atributo y se genera construye una cadena que
      * contiene el nombre del geter de dicho atributo. La cadena se construye de
      * la siguiente forma:
-     *
+     *<p>
      * geterName = la cadena "get" + el primer caracter de fieldName en
      * mayusculas + los caracteres restantates de la cadena fieldName.
-     *
+     *<p>
      * La construcción se realiza de esta respentando la nomenclatura camelCase.
      *
      * @param fieldName
@@ -96,18 +108,34 @@ public class Reflect {
      * @throws IllegalArgumentException
      *             El parametro para el seter es icompatible
      */
-    private Object invokeMethod(Class clazz, Object instance, Field field,
-            Object value, String methodName) throws SecurityException, 
+    private Object invokeMethod(Class clazz, Object instance, Class[] parametros,
+            Object value, String methodName) throws SecurityException,
             NoSuchMethodException, IllegalArgumentException,
             IllegalAccessException, InvocationTargetException {
 
-        Class[] parametros = {field.getType()};
         // Se obtien la referencia del seter del atributo
         Method seter = clazz.getMethod(methodName, parametros);
+        clazz.getMethod(methodName);
         // Se invoca al seter de la instancia correspondiente
         return seter.invoke(instance, value);
     }
-    
+
+    /**
+     *
+     * @param clazz
+     * @param instance
+     * @param methodName
+     * @return
+     */
+    private Object invokeMethod(Class clazz, Object instance, String methodName)
+            throws IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException {
+        
+        Method seter = clazz.getMethod(methodName);
+        clazz.getMethod(methodName);
+        return seter.invoke(instance);
+    }
+
     /**
      * Este método se encarga de de invocar al metodo set del atributo field,
      * perteneciente a la clase clazz ,cuya instancia es instance y establecerle
@@ -143,9 +171,10 @@ public class Reflect {
             InvocationTargetException {
 
         String seterName = getSeterName(field.getName());
-        invokeMethod(clazz, instance, field, value, seterName);
+        Class[] parametros = {field.getType()};
+        invokeMethod(clazz, instance, parametros, value, seterName);
     }
-    
+
     /**
      * Este método se encarga de de invocar al metodo get del atributo field,
      * perteneciente a la clase clazz ,cuya instancia es instance y establecerle
@@ -173,12 +202,11 @@ public class Reflect {
      * @throws IllegalArgumentException
      *             El parametro para el seter es icompatible
      */
-    public Object invokeGeter(Class clazz, Object instance, Field field
-         ) throws SecurityException, NoSuchMethodException,
+    public Object invokeGeter(Class clazz, Object instance, Field field) throws SecurityException, NoSuchMethodException,
             IllegalArgumentException, IllegalAccessException,
             InvocationTargetException {
 
         String geterName = getGeterName(field.getName());
-        return invokeMethod(clazz, instance, field, null, geterName);
+        return invokeMethod(clazz, instance, geterName);
     }
 }
--- src/py/com/mbaez/slam/annotation/Table.java
+++ src/py/com/mbaez/slam/annotation/Table.java
@@ -1,18 +1,28 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
  */
-
 package py.com.mbaez.slam.annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 /**
- *
- * @author mbaez
+ * @author Maximiliano Báez <mxbg.py@gmail.com>
+ * @version 1.0 17/10/2011
  */
-
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Table {
